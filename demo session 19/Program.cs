2e2aa7f1eaf6d981bd5e54ac6c50275075a7954f
Al-Saiem <Al-Saiem@DESKTOP-45MQB9U>
2024-07-19 23:52:27 +0300

    Add project files.

---
+++ demo session 19/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace demo_session_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Interface
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
